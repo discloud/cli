b57018f789d8d5b0fb087d42f866809c468a52dd
Gorniaky <hwss.s2@gmail.com>
2022-08-29 12:02:41 -0300

    support for plugins added

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { build } from "gluegun";
 export function run(argv: string[]) {
   const cli = build("discloud")
     .src(__dirname)
+    .plugins("./node_modules", { matching: "discloud-*", hidden: true })
     .defaultCommand()
     .help()
     .version()
