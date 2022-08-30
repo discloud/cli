8af7903ef45861a48157835ade319b5eb3fe5c0b
Gorniaky <hwss.s2@gmail.com>
2022-08-30 07:40:21 -0300

    constant `locales` created

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -3,3 +3,8 @@ import { filesystem } from "gluegun";
 export const logsPath = "discloud/logs";
 
 export const configPath = `${filesystem.homedir()}/.discloud`;
+
+export const locales = [
+  "en-US",
+  "pt-BR",
+];
