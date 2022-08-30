b7bbc21bfe8eeaca056b9cbdb81cbd486f2f0f8e
Gorniaky <hwss.s2@gmail.com>
2022-08-30 07:40:04 -0300

    `locales` command created

---
+++ src/commands/locale.ts
@@ -0,0 +1,36 @@
+import { RESTPutApiLocaleResult, Routes } from "@discloudapp/api-types/v2";
+import { GluegunCommand, GluegunToolbox } from "gluegun";
+import { apidiscloud, config } from "../util";
+import { locales } from "../util/constants";
+
+export default new class Locale implements GluegunCommand {
+  name = "locale";
+  description = "Set your locale";
+
+  async run(toolbox: GluegunToolbox) {
+    const { print, prompt } = toolbox;
+
+    if (!config.data.token)
+      return print.error("Please use login command before using this command.");
+
+    const { locale } = await prompt.ask({
+      name: "locale",
+      message: "Choose your locale",
+      type: "select",
+      choices: locales,
+    });
+
+    const res = await apidiscloud.put<RESTPutApiLocaleResult>(Routes.locale(locale), {});
+
+    if (res.status) {
+      if (res.status > 399)
+        return print.error(`[DISCLOUD API] ${res.data?.message}`);
+
+      if (res.data?.status === "ok") {
+        print.success(`[DISCLOUD API] ${res.data?.message ?? "Success"}!`);
+      } else {
+        print.warning(`[DISCLOUD API] ${res.data?.message}`);
+      }
+    }
+  }
+};
