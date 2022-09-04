979c1d39a9e1c8e4a6cb8eaa2863f51467933c92
Gorniaky <hwss.s2@gmail.com>
2022-09-04 19:48:32 -0300

    ModPermissions created

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -12,3 +12,14 @@ export const locales = [
 export const required_files = ["discloud.config"];
 
 export const requiredDiscloudConfigProps = ["MAIN", "TYPE", "RAM", "VERSION"];
+
+export const ModPermissions = {
+  backup_app: 1,
+  commit_app: 2,
+  edit_ram: 4,
+  logs_app: 8,
+  restart_app: 16,
+  start_app: 32,
+  status_app: 64,
+  stop_app: 128,
+};
