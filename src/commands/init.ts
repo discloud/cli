4b8d146528d95c6e9953080a82281eec6fcacde0
Gorniaky <hwss.s2@gmail.com>
2022-09-06 13:55:55 -0300

    init improved

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,4 +1,5 @@
 import { GluegunCommand, GluegunToolbox } from "gluegun";
+import { Apt } from "../util/constants";
 
 export default new class Init implements GluegunCommand {
   name = "init";
@@ -17,29 +18,95 @@ export default new class Init implements GluegunCommand {
         props: { appType: "bot", appAutoRestart: "false", appRam: 100, appVersion: "latest" },
       });
 
-    const { appId, appMain, appType } = await prompt.ask([{
-      name: "appType",
-      message: "Choose your app type",
-      type: "select",
-      choices: ["bot", "site"],
-    }, {
-      name: "appId",
-      message: "Your app id or subdomain",
-      type: "input",
-    }, {
-      name: "appMain",
-      message: "Input the path of main file",
-      type: "input",
-    }]);
+    const { app_apt, appMain, appType, appVersion } = await prompt.ask([
+      {
+        name: "appType",
+        message: "Choose your app type",
+        type: "select",
+        choices: ["bot", "site"],
+      }, {
+        name: "appMain",
+        message: "Input the path of main file",
+        type: "input",
+      }, {
+        name: "appVersion",
+        message: "Choose the version for your app",
+        type: "select",
+        choices: ["latest", "current", "suja"],
+        initial: 0,
+      }, {
+        name: "app_apt",
+        message: "Choose apt (use space to select)",
+        type: "multiselect",
+        choices: Object.entries(Apt).map(([name, value]) => ({
+          name,
+          hint: value.join(),
+          value: name,
+        })),
+        initial: 5,
+      },
+    ]);
+    /**
+     * Sorry, it is because app_apt is typed like string, not array
+     */
+    const appApt = [...app_apt].join();
 
-    const appAutoRestart = appType === "site";
-    const appRam = appType === "bot" ? 100 : 512;
-    const appVersion = appType === "bot" ? "latest" : "suja";
+    const minRam = appType === "site" ? 512 : 100;
+    let appRam;
+    do {
+      if (typeof appRam === "number")
+        print.warning(`RAM must to be greater than ${minRam}!`);
+
+      const { app_ram } = await prompt.ask({
+        name: "app_ram",
+        message: "Input the amount of RAM for your application",
+        type: "numeral",
+        initial: minRam,
+      });
+
+      appRam = parseInt(app_ram);
+    } while (appRam < (minRam));
+
+    let appId;
+    if (appType === "site") {
+      const { app_id } = await prompt.ask({
+        name: "app_id",
+        message: `Input the ${appType === "site" ? "subdomain" : "app id"}`,
+        type: "input",
+      });
+
+      if (appType === "site")
+        appId = app_id.match(appType === "site" ? /(\w+)(?:\.discloud\..*)?/ : /(.+)/)?.[1];
+    }
+
+    let appName;
+    let appAvatar;
+    if (appType === "bot") {
+      const { app_name } = await prompt.ask({
+        name: "app_name",
+        message: "App name",
+        type: "input",
+      });
+      appName = app_name;
+
+      do {
+        if (appAvatar)
+          print.warning("This must be like URL with extensions JPG, JPEG or PNG.");
+
+        const { app_avatar } = await prompt.ask({
+          name: "app_avatar",
+          message: "Avatar URL",
+          type: "input",
+        });
+
+        appAvatar = app_avatar;
+      } while (!/^(https?:\/\/.+\.(?:jpg|jpeg|png))(?:\?.*)?$/.test(appAvatar));
+    }
 
     template.generate({
-      template: "discloud.config.ejs",
+      template: `discloud.config.${appType}.ejs`,
       target: "discloud.config",
-      props: { appId, appMain, appType, appAutoRestart, appRam, appVersion },
+      props: { appApt, appAvatar, appId, appMain, appName, appType, appRam, appVersion },
     });
   }
 };
