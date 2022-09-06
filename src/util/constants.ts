77d0d017970e3a58ad6ebde4edc7311cadab5b1a
Gorniaky <hwss.s2@gmail.com>
2022-09-06 13:55:12 -0300

    apt constant created

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -4,18 +4,15 @@ export const logsPath = "discloud/logs";
 
 export const configPath = `${filesystem.homedir()}/.discloud`;
 
-export const locales = [
-  "en-US",
-  "pt-BR",
-];
+export const locales = ["en-US", "pt-BR"];
 
 export const blocked_files = {
   go: [".git", ".vscode"],
-  js: [".git", ".vscode", "node_modules", "package-lock.json"],
+  js: [".git", ".vscode", "node_modules", "package-lock.json", "yarn.lock"],
   py: [".git", ".vscode"],
   rb: [".git", ".vscode", "Gemfile.lock"],
   rs: [".git", ".vscode", "Cargo.lock", "target"],
-  ts: [".git", ".vscode", "node_modules", "package-lock.json"],
+  ts: [".git", ".vscode", "node_modules", "package-lock.json", "yarn.lock"],
 };
 
 export const required_files = {
@@ -38,4 +35,29 @@ export const ModPermissions = {
   start_app: 32,
   status_app: 64,
   stop_app: 128,
+};
+
+export const Apt = {
+  canvas: [
+    "libcairo2-dev",
+    "libpango1.0-dev",
+    "libjpeg-dev",
+    "libgif-dev",
+    "librsvg2-dev",
+    "libgbm-dev",
+  ],
+  puppeteer: [
+    "libnss3",
+    "libatk-bridge2.0-0",
+    "libgtk-3-0",
+    "libasound2",
+    "libxshmfence-dev",
+    "libxshmfence-dev",
+    "libdrm-dev",
+    "libgbm-dev",
+  ],
+  java: ["default-jre"],
+  ffmpeg: ["ffmpeg"],
+  libgl: ["libsm6", "libxext6"],
+  tools: ["git", "wget", "make", "openssl", "curl"],
 };
