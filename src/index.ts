29a687797a4e8131c1a1853b6b98d9cdfa598db2
Gorniaky <hwss.s2@gmail.com>
2022-09-12 14:26:44 -0300

    feat: sentry config

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
+import * as Sentry from "@sentry/node";
+import "@sentry/tracing";
 import { build } from "gluegun";
+
+Sentry.init({
+  dsn: "https://c7832919af874c068ce1ffe90177f470@sentry.discloudbot.com/4",
+  tracesSampleRate: 1.0,
+});
 
 export function run(argv: string[]) {
   const cli = build("discloud")
