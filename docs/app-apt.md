913bf571498640fb03cc1f19d27ac8ac359a2e4c
Gorniaky <hwss.s2@gmail.com>
2022-09-18 22:45:47 -0300

    feat: Create command to install and uninstall apt packages

---
+++ docs/app-apt.md
@@ -0,0 +1,32 @@
+# App:Apt
+
+Install or uninstall apt packages for you application.
+
+* [app:apt](#appapt)
+
+```sh-session
+USAGE
+  $ discloud apps:apt [APP_ID] -i=PACKAGE
+
+ARGUMENTS
+  APP_ID The id of your app on Discloud
+
+OPTIONS
+  -i, --install [PACKAGE]   Install a package.
+  -u, --uninstall [PACKAGE] Uninstall a package.
+
+EXAMPLE
+  $ discloud app:apt APP_ID -i=PACKAGE
+  $ discloud app:apt APP_ID -u=PACKAGE
+
+PACKAGE
+  - canvas
+  - puppeteer
+  - java
+  - ffmpeg
+  - libgl
+  - openssl
+  - tools
+```
+
+* If does not contains `APP_ID`, the CLI will show a list of your apps for you to select.
---
+++ src/commands/app-apt.ts
@@ -0,0 +1,105 @@
+import { RESTGetApiAppAllResult, RESTPutApiAppAptResult, Routes } from "@discloudapp/api-types/v2";
+import { GluegunCommand, GluegunToolbox } from "gluegun";
+import { exit } from "node:process";
+import { apidiscloud, config, RateLimit } from "../util";
+import { Apt } from "../util/constants";
+
+export default new class AppApt implements GluegunCommand {
+  name = "app:apt";
+  description = "Install or uninstall apt packages for you application.";
+
+  async run(toolbox: GluegunToolbox) {
+    const { parameters, print, prompt } = toolbox;
+
+    if (!config.data.token)
+      return print.error("Please use login command before using this command.");
+
+    if (RateLimit.isLimited)
+      return print.error(`Rate limited until: ${RateLimit.limited}`);
+
+    if (!parameters.first) {
+      const spin = print.spin({
+        text: print.colors.cyan("Fetching apps..."),
+      });
+
+      const apiRes = await apidiscloud.get<RESTGetApiAppAllResult>(Routes.app("all"));
+
+      spin.stop();
+
+      if (apiRes.data)
+        if ("apps" in apiRes.data) {
+          const { appId } = await prompt.ask({
+            name: "appId",
+            message: "Choose the app",
+            type: "select",
+            choices: [{
+              name: "all",
+              message: "All apps",
+              value: "all",
+            }].concat(apiRes.data.apps.map(app => ({
+              name: app.id,
+              message: `${app.name} - ${app.id} - ${app.online ?
+                print.colors.green("online") :
+                print.colors.red("offline")}`,
+              value: app.id,
+            }))),
+          });
+
+          parameters.first = appId;
+        }
+
+      if (!parameters.first)
+        return print.error("Need app id.");
+    }
+
+    let method: "delete" | "put" | undefined;
+
+    if (Object.keys(parameters.options).length)
+      method =
+        (parameters.options.u ?? parameters.options.uninstall) ? "delete" :
+          (parameters.options.i ?? parameters.options.install) ? "put" :
+            method;
+
+    if (!method || !Object.keys(Apt).includes(parameters.options[method]))
+      return print.error(
+        "You need to use one of the options below:" +
+        "\n  -i, --install [PACKAGE]   Install a package." +
+        "\n  -u, --uninstall [PACKAGE] Uninstall a package." + "\n" +
+        "\nPACKAGES:" + "\n" +
+        Object.keys(Apt).map(pkg => `  - ${pkg} ${Apt[<"tools">pkg]}`).join("\n"),
+      );
+
+    const apt = parameters.options[method];
+
+    let action;
+    switch (method) {
+      case "delete":
+        action = `Uninstalling ${apt} from ${parameters.first} app...`;
+        break;
+      case "put":
+        action = `Installing ${apt} for ${parameters.first} app...`;
+        break;
+      default:
+        action = "";
+        break;
+    }
+
+    const spin = print.spin({
+      text: print.colors.cyan(action),
+    });
+
+    const apiRes = await apidiscloud[method]<
+      RESTPutApiAppAptResult
+    >(Routes.appApt(parameters.first),
+      ["delete", "put"].includes(method) ? {
+        apt,
+      } : undefined);
+
+    new RateLimit(apiRes.headers);
+
+    if (apiRes.status)
+      if (print.spinApiRes(apiRes, spin) > 399) return exit(apiRes.status);
+
+    exit(0);
+  }
+};
