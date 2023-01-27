495cbbe23407a24f29f1d12573dd88715960c952
Gorniaky <hwss.s2@gmail.com>
2023-01-26 22:47:15 -0300

    .

--- src/extensions/FetchAndAskForApps.ts
+++ src/extensions/FetchAndAskForApps.ts
@@ -4,9 +4,7 @@ import { apidiscloud, RateLimit } from "../util";
 
 export default function (toolbox: GluegunToolbox) {
   toolbox.prompt.fetchAndAskForApps = async function (options) {
-    const spin = toolbox.print.spin({
-      text: toolbox.print.colors.cyan("Fetching apps..."),
-    });
+    const spin = toolbox.print.spin({ text: toolbox.print.colors.cyan("Fetching apps...") });
 
     const apiRes = await apidiscloud.get<RESTGetApiAppAllResult | RESTGetApiTeamResult>(options?.url ?? Routes.app("all"));
 
@@ -14,13 +12,7 @@ export default function (toolbox: GluegunToolbox) {
 
     spin.stop();
 
-    if (apiRes.data?.apps)
-      return toolbox.prompt.askForApps(apiRes.data.apps, {
-        all: options?.all,
-        discloudConfig: options?.discloudConfig,
-        discloudConfigPath: options?.discloudConfigPath,
-        showStatus: options?.showStatus,
-      });
+    if (apiRes.data?.apps) return toolbox.prompt.askForApps(apiRes.data.apps, options);
 
     return {};
   };
