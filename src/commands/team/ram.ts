f4184dc6821149613ed3736bfa88cb9a26e90ad2
Gorniaky <hwss.s2@gmail.com>
2025-03-09 19:49:27 -0300

    feat: create ram command

---
+++ src/commands/team/ram.ts
@@ -0,0 +1,37 @@
+import { type RESTPutApiAppRamResult, Routes } from "@discloudapp/api-types/v2";
+import { type CommandInterface } from "../../interfaces/command";
+
+interface CommandArgs {
+  amount: number
+  app: string
+}
+
+export default <CommandInterface<CommandArgs>>{
+  name: "ram <app> <amount>",
+  description: "Set amount of ram for your app",
+
+  requireAuth: true,
+
+  options: {
+    amount: {
+      type: "number",
+      coerce(arg) {
+        if (arg >= 100) return arg;
+      },
+    },
+    app: {
+      type: "string",
+      description: "App id",
+    },
+  },
+
+  async run(core, args) {
+    core.print.spin(`Changing amount of ${args.app} RAM to ${args.amount}...`);
+
+    const response = await core.api.put<RESTPutApiAppRamResult>(Routes.teamRam(args.app), {
+      body: { ramMB: args.amount },
+    });
+
+    core.print.apiResponse(response);
+  },
+};
