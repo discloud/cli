465794f0670af15feeaf3a532786ef15cb46cbd8
Gorniaky <hwss.s2@gmail.com>
2025-03-09 17:41:32 -0300

    feat: create app status command

---
+++ src/commands/app/status.ts
@@ -0,0 +1,39 @@
+import { type RESTGetApiAppAllStatusResult, type RESTGetApiAppStatusResult, Routes } from "@discloudapp/api-types/v2";
+import { type CommandInterface } from "../../interfaces/command";
+
+interface CommandArgs {
+  app: string
+}
+
+export default <CommandInterface<CommandArgs>>{
+  name: "status [app]",
+  description: "Get status of your apps",
+
+  requireAuth: true,
+
+  options: {
+    app: {
+      type: "string",
+      description: "App id",
+      default: "all",
+    },
+  },
+
+  async run(core, args) {
+    const spinner = core.print.spin(`Fetching your app${args.app === "all" ? "s" : ""} status...`);
+
+    const response = await core.api.get<
+      | RESTGetApiAppStatusResult
+      | RESTGetApiAppAllStatusResult
+    >(Routes.appStatus(args.app));
+
+    spinner.stop();
+
+    if (response.apps) {
+      const apps = Array.isArray(response.apps) ? response.apps : [response.apps];
+      core.print.table(apps, ["netIO"]);
+    } else {
+      core.print.apiResponse(response);
+    }
+  },
+};
