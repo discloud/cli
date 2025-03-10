a3df801348474f9906fbcc08364bf9701957ae0b
Gorniaky <hwss.s2@gmail.com>
2025-03-10 19:10:27 -0300

    fix user agent prefix

--- src/services/discloud/UserAgent.ts
+++ src/services/discloud/UserAgent.ts
@@ -2,15 +2,15 @@ import { arch, platform, release, type } from "os";
 
 export class UserAgent {
   constructor(readonly version: string, readonly prefix?: string) {
-    prefix ??= "DiscloudCLI";
+    this.prefix ??= "DiscloudCLI";
   }
-
-  #userAgent!: string;
 
   #getUserAgent() {
     const osRelease = release().split?.(".").slice(0, 2).join(".") ?? release();
     return `${this.prefix}/${this.version} (${type()} ${osRelease}; ${platform()}; ${arch()})`;
   }
+
+  #userAgent!: string;
 
   toString() {
     return this.#userAgent ??= this.#getUserAgent();
