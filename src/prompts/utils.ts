aab83d5958ec4d985220720518c6901c91f008c4
Gorniaky <hwss.s2@gmail.com>
2025-03-17 19:56:30 -0300

    .

--- src/prompts/utils.ts
+++ src/prompts/utils.ts
@@ -4,10 +4,8 @@ export async function promptTrier<R>(fn: () => Promise<R>) {
   try {
     return await fn();
   } catch (error) {
-    if (error instanceof Error) {
-      if (error.name === "ExitPromptError")
-        throw new PromptError("Cancelled");
-    }
+    if (error instanceof Error && error.name === "ExitPromptError")
+      throw new PromptError("Cancelled");
 
     throw error;
   }
