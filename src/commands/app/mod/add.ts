93c9d8926f3cc560be2ba8eec4aa3b183237fd2b
Gorniaky <30408913+Gorniaky@users.noreply.github.com>
2025-03-23 12:02:34 -0300

    fix(command-app-mod): fix request body (#599)

--- src/commands/app/mod/add.ts
+++ src/commands/app/mod/add.ts
@@ -1,11 +1,11 @@
 import { type RESTPostApiAppTeamResult, Routes } from "@discloudapp/api-types/v2";
-import { ModPermissionsBF } from "@discloudapp/util";
+import { ModPermissionsBF, type ModPermissionsString } from "@discloudapp/util";
 import { type ICommand } from "../../../interfaces/command";
 
 interface CommandArgs {
   app: string
   mod: string
-  perms: string[]
+  perms: ModPermissionsString[]
 }
 
 export default <ICommand<CommandArgs>>{
@@ -33,7 +33,12 @@ export default <ICommand<CommandArgs>>{
   async run(core, args) {
     core.print.spin(`Adding MOD ${args.mod} to ${args.app}...`);
 
-    const response = await core.api.post<RESTPostApiAppTeamResult>(Routes.appTeam(args.app));
+    const response = await core.api.post<RESTPostApiAppTeamResult>(Routes.appTeam(args.app), {
+      body: {
+        modID: args.mod,
+        perms: args.perms,
+      },
+    });
 
     if (!response.app) return core.print.apiResponse(response);
 
