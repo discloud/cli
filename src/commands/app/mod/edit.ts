4c52d78c787dd6f263527da72d9727bca22d8cad
Gorniaky <hwss.s2@gmail.com>
2025-03-23 11:52:52 -0300

    fix(command-app-mod): fix request body

--- src/commands/app/mod/edit.ts
+++ src/commands/app/mod/edit.ts
@@ -1,11 +1,11 @@
 import { type RESTPutApiAppTeamResult, Routes } from "@discloudapp/api-types/v2";
-import { ModPermissionsBF } from "@discloudapp/util";
+import { ModPermissionsBF, type ModPermissionsString } from "@discloudapp/util";
 import { type ICommand } from "../../../interfaces/command";
 
 interface CommandArgs {
   app: string
   mod: string
-  perms: string[]
+  perms: ModPermissionsString[]
 }
 
 export default <ICommand<CommandArgs>>{
@@ -33,7 +33,12 @@ export default <ICommand<CommandArgs>>{
   async run(core, args) {
     core.print.spin(`Editting MOD ${args.mod} perms to ${args.app}...`);
 
-    const response = await core.api.put<RESTPutApiAppTeamResult>(Routes.appTeam(args.app));
+    const response = await core.api.put<RESTPutApiAppTeamResult>(Routes.appTeam(args.app), {
+      body: {
+        modID: args.mod,
+        perms: args.perms,
+      },
+    });
 
     if (!response.app) return core.print.apiResponse(response);
 
