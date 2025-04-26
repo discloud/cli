5bbde5f27921e48ad0bffffbccd2aff744fdfe82
Gorniaky <hwss.s2@gmail.com>
2025-04-26 14:54:16 -0300

    refactor(deps): replace jsonwebtoken with jose for JWT decoding

--- src/services/discloud/utils.ts
+++ src/services/discloud/utils.ts
@@ -1,8 +1,8 @@
-import { decode } from "jsonwebtoken";
+import { decodeJwt } from "jose";
 
 export function tokenIsDiscloudJwt(token: string): boolean {
   try {
-    const payload = decode(token, { json: true });
+    const payload = decodeJwt(token);
     return payload && "id" in payload && "key" in payload || false;
   } catch { }
   return false;
