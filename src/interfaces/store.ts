a594d69bf3cbb2f33ddc731dbb3f0b81ad396e0b
Gorniaky <hwss.s2@gmail.com>
2025-06-04 17:52:32 -0300

    refactor(store): enhance type definitions for NestedStoreData and NestedStoreKeys

--- src/interfaces/store.ts
+++ src/interfaces/store.ts
@@ -1,26 +1,55 @@
 export type DefaultNestChar = "."
 
-export type NestedStoreData<Obj, Key, NestChar extends string = DefaultNestChar> =
-  Key extends string
-  ? Key extends `${infer Segment}${NestChar}${infer Rest}`
-  ? NestedStoreData<NestedData<Obj, Segment>, Rest, NestChar>
-  : NestedData<Obj, Key>
-  : NestedData<Obj, Key>
+export type NestedStoreData<O, K, NestChar extends string = DefaultNestChar> =
+  K extends string
+  ? K extends `${infer Segment}${NestChar}${infer Rest}`
+  ? NestedStoreData<NestedData<O, Segment>, Rest, NestChar>
+  : NestedData<O, K>
+  : NestedData<O, K>
 
-type NestedData<Obj, Key> =
-  Key extends keyof Obj
-  ? Obj[Key]
-  : Key extends `${number}`
+type NestedData<O, K> =
+  K extends keyof O
+  ? O[K]
+  : K extends `${number}`
   // @ts-expect-error ts(2536)
-  ? Obj[Key]
+  ? O[K]
   : void
 
-export type NestedStoreKeys<Obj, Key extends keyof Obj = keyof Obj, NestChar extends string = DefaultNestChar> =
-  Key extends string
-  ? Obj[Key] extends Record<string, any>
-  ? Key | `${Key}${NestChar}${NestedStoreKeys<Obj[Key]>}`
-  : Key
-  : string
+export interface PathsOptions {
+  bracketNotation?: boolean
+  leavesOnly?: boolean
+}
+
+export type NestedStoreKeys<O, Options extends PathsOptions = {}> = _NestedStoreKeys<Required<O>, Options>
+
+type Primitive = string | number | bigint | boolean | symbol | null | undefined
+
+type _NestedStoreKeys<O, Options extends PathsOptions> = {
+  [K in keyof O]:
+  O[K] extends Primitive
+  ? K & string
+  : O[K] extends Array<infer V>
+  ? Options["bracketNotation"] extends true
+  ? Options["leavesOnly"] extends true
+  ? `${K & string}[${number}].${NestedStoreKeys<V, Options>}`
+  :
+  | K & string
+  | `${K & string}[${number}]`
+  | `${K & string}[${number}].${NestedStoreKeys<V, Options>}`
+  : Options["leavesOnly"] extends true
+  ? `${K & string}.${number}.${NestedStoreKeys<V, Options>}`
+  :
+  | K & string
+  | `${K & string}.${number}`
+  | `${K & string}.${number}.${NestedStoreKeys<V, Options>}`
+  : O extends object
+  ? Options["leavesOnly"] extends true
+  ? `${K & string}.${NestedStoreKeys<O[K], Options>}`
+  :
+  | K & string
+  | `${K & string}.${NestedStoreKeys<O[K], Options>}`
+  : never
+}[keyof O];
 
 export interface IStore<T extends object> {
   delete<K extends NestedStoreKeys<T>>(key: K): void
