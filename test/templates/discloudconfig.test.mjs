4be183dc0170cff0953f27c85cb95af9efe8ed59
Gorniaky <hwss.s2@gmail.com>
2025-06-23 00:44:29 -0300

    refactor: update discloud.config.ejs template to handle null values and add corresponding test

---
+++ test/templates/discloudconfig.test.mjs
@@ -0,0 +1,23 @@
+import { renderFile } from "ejs";
+import { suite, test } from "node:test";
+import { join } from "path";
+
+suite("template DiscloudConfig test", async () => {
+  await test("rendering Discloud Config", async (t) => {
+    const templateFilePath = join("templates", "discloud.config.ejs");
+
+    const props = {
+      Undefined: undefined,
+      Null: null,
+      Number: 0,
+      EmptyString: "",
+      String: "String",
+    };
+
+    const expected = "# https://docs.discloudbot.com/discloud.config\nNumber=0\nEmptyString=\nString=String\n";
+
+    const rendered = await renderFile(templateFilePath, { props }, { async: true });
+
+    t.assert.strictEqual(rendered.replace(/\r/g, ""), expected);
+  });
+});
